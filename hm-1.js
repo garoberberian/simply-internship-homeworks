8b439eca484f78292b0beb2e80b13aaadc47fd1a
Garo <96496626+garoberberian@users.noreply.github.com>
2022-04-22 00:33:20 -0700

    Add files via upload

---
+++ hm-1.js
@@ -0,0 +1,48 @@
+// ==============================
+// Don't touch
+// ==============================
+Array.prototype.forEach = null;
+Array.prototype.map = null;
+// ==============================
+
+// Implement these methods
+
+Array.prototype.find = function () {
+  // implementation
+};
+
+Array.prototype.findIndex = function () {
+  // implementation
+};
+
+Array.prototype.lastIndexOf = function () {
+  // implementation
+};
+
+Array.prototype.some = function () {
+  // implementation
+};
+
+Array.prototype.every = function () {
+  // implementation
+};
+
+Array.prototype.reduce = function () {
+  // implementation
+};
+
+Array.prototype.reduceRight = function () {
+  // implementation
+};
+
+Array.prototype.join = function () {
+  // implementation
+};
+
+Array.prototype.pop = function () {
+  // implementation
+};
+
+Array.prototype.unshift = function () {
+  // implementation
+};
