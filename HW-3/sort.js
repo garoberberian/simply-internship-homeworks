3419b82aabe50be0af8ed2c8f4f2c5c8e7573a4a
Garo <96496626+garoberberian@users.noreply.github.com>
2022-05-03 14:39:20 -0700

    Update sort.js

--- HW-3/sort.js
+++ HW-3/sort.js
@@ -1,10 +1,8 @@
 const sort = input => {
-    const min = Math.min(...input);
-    const max = Math.max(...input);
     const map = new Map();
     let inputClone = [...input];
 
-    for(let i = min; i <= max; i++) {
+    for(let i = Math.min(...input); i <= Math.max(...input); i++) {
         if(inputClone.includes(i)) {
             map.has(i) && map.set(i, [].concat(map.get(i), i));
            !map.has(i) && map.set(i, i);
