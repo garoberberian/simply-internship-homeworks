29b7aea1b989b6793c20d4bb9f1fea5f3c8c72b8
Garo <96496626+garoberberian@users.noreply.github.com>
2022-05-23 01:32:09 +0400

    Update index.js

--- HW-5/index.js
+++ HW-5/index.js
@@ -1,28 +1,51 @@
-import { fetchData } from "./fetchData.js";
-
+'use strict'
 const navbarTitle = document.querySelector('.navbar__title');
 const plansTexts = document.querySelectorAll('.plans-text');
 const shapes = document.querySelectorAll('.shape');
+const servicesTitles = document.querySelectorAll('.services__title');
+const systemNames = document.querySelectorAll('.system-name');
+const ids = document.querySelectorAll('.id');
+const createdDate = document.querySelectorAll('.created-date');
+const activeLicenses = document.querySelectorAll('.active-licenses');
 
-const showNavbarTexts = ({page_title, plans}) => {
+const fetchData = async (path) => {
+    const response = await fetch(path);
+    return response.json();
+}
+
+const insertNavbarTexts = ({page_title, plans}) => {
     navbarTitle.innerHTML = page_title;
-    const planNames = plans.map(plan => plan.name.toUpperCase());
-
-    plansTexts.forEach((plansText, i) => {
-        plansText.innerHTML = planNames[i];
-
-        if(!plans[i].active) {
+    
+    plans.forEach((plan, i) => {
+        plansTexts[i].innerHTML = plan.name;
+        if(!plan.active) {
             shapes[i].style.opacity = 0.25;
-            plansText.style.opacity = 0.5;
+            plansTexts[i].style.opacity = 0.5;
         }
-    } )
+    })
+
+}
+
+const insertSystemsTexts = (systems) => {
+    systems.forEach((system, i) => {
+        systemNames[i].innerHTML = system.system_name;
+        ids[i].innerHTML = system.id;
+        createdDate[i].innerHTML = system.created_date;
+        activeLicenses[i].innerHTML = system.active_licenses;
+    })
+}
+
+const insertTabsTexts = ({tabs}) => {
+    tabs.forEach((tab, i) => {
+        servicesTitles[i].innerHTML = tab.title;
+        const tabDataLength = Object.keys(tab.data).length;
+        if(tabDataLength) {
+            insertSystemsTexts(tab.data.systems)
+        }
+    });
 }
 
 fetchData('./test-data.json').then(data => {
-    console.log(data)
-    showNavbarTexts(data);
-} );
-
-
-
-
+    insertNavbarTexts(data);
+    insertTabsTexts(data);
+});
