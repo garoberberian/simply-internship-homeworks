a032d47a025d87bb2317c9f0ebd98d518af1df25
Garo <96496626+garoberberian@users.noreply.github.com>
2022-05-22 18:17:33 +0400

    Update index.js

--- HW-5/index.js
+++ HW-5/index.js
@@ -2,13 +2,27 @@ import { fetchData } from "./fetchData.js";
 
 const navbarTitle = document.querySelector('.navbar__title');
 const plansTexts = document.querySelectorAll('.plans-text');
+const shapes = document.querySelectorAll('.shape');
+
+const showNavbarTexts = ({page_title, plans}) => {
+    navbarTitle.innerHTML = page_title;
+    const planNames = plans.map(plan => plan.name.toUpperCase());
+
+    plansTexts.forEach((plansText, i) => {
+        plansText.innerHTML = planNames[i];
+
+        if(!plans[i].active) {
+            shapes[i].style.opacity = 0.25;
+            plansText.style.opacity = 0.5;
+        }
+    } )
+}
 
 fetchData('./test-data.json').then(data => {
-    const {page_title, plans, tabs} = data;
-    navbarTitle.innerHTML = page_title;
-    console.log(plans)
-    const planNames = plans.map(plan => plan.name)
-    plansTexts.forEach((plansText, i) => plansText.innerHTML = planNames[i])
-})
+    console.log(data)
+    showNavbarTexts(data);
+} );
+
+
 
 
